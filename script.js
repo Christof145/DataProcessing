ae8a2d565b669b57ec2779a504d6e29cebf197fa
Christof145 <43606559+Christof145@users.noreply.github.com>
2022-05-09 09:32:49 +0200

    add visualization

--- script.js
+++ script.js
@@ -30,6 +30,34 @@ const userAction = async () => {
             'Accept': 'application/xml'
         }
     });
+    // XML Calls 2
+    const addressHappynessCountry2  = "http://localhost:3000/worldHappyness/countryHappyness/" + country2;
+        const responseHappynessCountry2 = await fetch(addressHappynessCountry2 , {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/xml',
+                    'Accept': 'application/xml'
+                }
+            });
+
+    const addressAlcoholCountry2  = "http://localhost:3000/worldAlcohol/countryalcohol/" + country2;
+    const responseAlcoholCountry2 = await fetch(addressAlcoholCountry2 , {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/xml',
+            'Accept': 'application/xml'
+        }
+    });
+    
+    const addressGDPCountry2  = "http://localhost:3000/worldGDP/countrygdp/" + country2;
+    const responseGDPXmlCountry2 = await fetch(addressGDPCountry2 , {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/xml',
+            'Accept': 'application/xml'
+        }
+    });
+    
 
     //Json
     const addressHappynessJson  = "http://localhost:3000/worldHappyness/countryHappyness/" + country2;
@@ -75,31 +103,48 @@ const suicideRate2016 = await GDPJson[0]['Suicide_Rate_2016'];
 
 
 // XML Text
-var GDPXml = await responseGDPXml.text();
-var AlcoholXml = await responseAlcohol.text();
-var happynessXml = await responseHappyness.text();
+var GDPXmlCountry1 = await responseGDPXml.text();
+var AlcoholXmlCountry1 = await responseAlcohol.text();
+var happynessXmlCountry1 = await responseHappyness.text();
+
+var GDPXmlCountry2 = await responseGDPXmlCountry2.text();
+var AlcoholXmlCountry2 = await responseAlcoholCountry2.text();
+var happynessXmCountry2 = await responseHappynessCountry2.text();
 
 // XML text parsers
-let parserG = new DOMParser();
-let parserH = new DOMParser();
-let parserAlcohol = new DOMParser();
+let parserGDPCountry1 = new DOMParser();
+let parserHappynessCountry1 = new DOMParser();
+let parserAlcoholCountry1 = new DOMParser();
+
+let parserGDPCountry2 = new DOMParser();
+let parserHappynessCountry2 = new DOMParser();
+let parserAlcoholCountry2 = new DOMParser();
 
 // Parsed strings
-let xmlDocH = await parserH.parseFromString(happynessXml,"text/xml");
-let xmlDocG = await parserG.parseFromString(GDPXml,"text/xml");
-let xmlDocAlcohol = await parserAlcohol.parseFromString(AlcoholXml,"text/xml");
+let xmlDocH = await parserHappynessCountry1.parseFromString(happynessXmlCountry1,"text/xml");
+let xmlDocG = await parserGDPCountry1.parseFromString(GDPXmlCountry1,"text/xml");
+let xmlDocA = await parserAlcoholCountry1.parseFromString(AlcoholXmlCountry1,"text/xml");
+
+let xmlDocH2 = await parserHappynessCountry2.parseFromString(happynessXmCountry2,"text/xml");
+let xmlDocG2 = await parserGDPCountry2.parseFromString(GDPXmlCountry2,"text/xml");
+let xmlDocA2 = await parserAlcoholCountry2.parseFromString(AlcoholXmlCountry2,"text/xml");
 
 // Get specific elements in xml
 var Srate = xmlDocG.getElementsByTagName("Suicide_Rate_2016")[0].textContent;
-var AlcoholTotal = xmlDocAlcohol.getElementsByTagName("total_liters_of_alcohol")[0].textContent;
+var AlcoholTotal = xmlDocA.getElementsByTagName("total_liters_of_alcohol")[0].textContent;
 var familyValue = xmlDocH.getElementsByTagName("Family")[0].textContent;
+
+var Srate2 = xmlDocG2.getElementsByTagName("Suicide_Rate_2016")[0].textContent;
+var AlcoholTotal2 = xmlDocA2.getElementsByTagName("total_liters_of_alcohol")[0].textContent;
+var familyValue2 = xmlDocH2.getElementsByTagName("Family")[0].textContent;
 
 // console.log(happynessXml);
 // console.log(AlcoholXml);
 // console.log(GDPXml);
 
 // console.log(xmlDocH);
-// console.log(xmlDocG);
+console.log(xmlDocG);
+console.log(xmlDocG2);
 // console.log(xmlDocAlcohol);
 
 // console.log(Srate);
@@ -107,42 +152,18 @@ var familyValue = xmlDocH.getElementsByTagName("Family")[0].textContent;
 // console.log(AlcoholTotal);
 
 
-
-var xValues = ["Alcohol", "Family Rate", "Suicide Rate"];
-var yValues = [AlcoholTotal, familyValue, Srate];
-var barColors = ["red", "green","blue"];
-
-new Chart("myChart", {
-    type: "bar",
-    data: {
-    labels: xValues,
-    datasets: [{
-    backgroundColor: barColors,
-    data: yValues
-    }]
-    },
-    options: {
-    legend: {display: false},
-    title: {
-    display: true,
-    text: "Wlcome to comparing "
-    }
-    }
-});
-var xValues = ["Alcohol", "Family Rate", "Suicide Rate"];
-var nameValues = "Hunagry"
+var xValues = ["Total liters of Alcohol", "Family Rate", "Suicide Rate"];
 new Chart("myChart", {
   type: "line",
   data: {
     labels: xValues,
     datasets: [{
       data: [AlcoholTotal,familyValue, Srate],
-      labels: "Hungry",
       borderColor: "red",
       fill: false,
       labels: country1,
     },{
-      data: [15,17, 12],
+      data: [AlcoholTotal2, familyValue2, Srate2],
       borderColor: "green",
       fill: false
     }]
